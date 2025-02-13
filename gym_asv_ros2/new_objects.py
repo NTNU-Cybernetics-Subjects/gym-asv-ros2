8f99fc4e6ed1a5da360a38a1db2fd999cbb173a9
Vegar <Vegar1201@live.no>
2025-02-13 19:18:18 +0100

    Add a camera position variable that things are draw according to.

---
+++ gym_asv_ros2/new_objects.py
@@ -0,0 +1,62 @@
+import pyglet
+import shapely.geometry
+import shapely.affinity
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+class BaseObstacle:
+    @property
+    def boundary(self) -> shapely.Geometry:
+        return NotImplemented
+
+    @abstractmethod
+    def update(self) -> None:
+        pass
+
+    @property
+    def pyglet_shape(self) -> pyglet.shapes.ShapeBase:
+        raise NotImplementedError
+
+    @abstractmethod
+    def init_pyglet_shape(self, scale: int, batch: pyglet.graphics.Batch) -> None:
+        raise NotImplementedError
+
+
+class CircularObstacle(BaseObstacle):
+    def __init__(self, position: np.ndarray, radius: float) -> None:
+        self.position = position
+        self.radius = radius
+        self._boundary: shapely.geometry.LineString
+        self.init_boundary()
+
+        self._pyglet_shape: pyglet.shapes.Circle
+
+    @property
+    def boundary(self) -> shapely.geometry.LineString:
+        return self._boundary
+
+
+    def init_boundary(self) -> None:
+        full_circle = shapely.geometry.Point(*self.position).buffer(self.radius)
+        # Simplify the circle representation
+        self._boundary = full_circle.boundary.simplify(0.3, preserve_topology=False)  # pyright: ignore
+
+    def init_pyglet_shape(self, scale: float, batch: pyglet.graphics.Batch) -> None:
+        scaled_position = self.position * scale
+        scaled_radius = self.radius * scale
+        self._pyglet_shape = pyglet.shapes.Circle(
+            scaled_position[0], scaled_position[1], scaled_radius, batch=batch
+        )
+
+    def update_pyglet_position(self, camera_position: np.ndarray) -> None:
+        screen_position = camera_position + self.position
+        self._pyglet_shape.position = screen_position.tolist()
+
+    @property
+    def pyglet_shape(self) -> pyglet.shapes.Circle:
+        return self._pyglet_shape
+
+
+if __name__ == "__main__":
+    c = CircularObstacle(np.array([0, 0]), 2)
--- gym_asv_ros2/visualization.py
+++ gym_asv_ros2/visualization.py
@@ -1,12 +1,13 @@
 from pathlib import Path
-import time
+# import time
 import pyglet
 import shapely.affinity
 import numpy as np
-from gym_asv_ros2.obstacles import BaseObstacle
+# from gym_asv_ros2.obstacles import BaseObstacle
+from gym_asv_ros2.new_objects import BaseObstacle, CircularObstacle
 from gym_asv_ros2.vessel import Vessel
 from gym_asv_ros2.simulator import Game
-from pyglet.window import key
+# from pyglet.window import key
 
 
 class visualizer:
@@ -18,22 +19,29 @@ class visualizer:
         self.geoms = []
 
         self.bg_sprite = None
-        # self.bg_position_corretion = 0
-
 
         self.pixels_per_unit = self.window.width / 100
         self.window_origo = (self.window.width / 2, self.window.height / 2)
 
+        self.camera_position = np.array([0,0])
+
     def add_agent(self, agent_shape: shapely.geometry.Polygon):
+        # Shape should be defined in origo
         scaled_agent_shape = shapely.affinity.scale(
-            agent_shape, self.pixels_per_unit, self.pixels_per_unit
+            agent_shape, self.pixels_per_unit, self.pixels_per_unit, origin=(0,0)
         )
         self.agent = pyglet.shapes.Polygon(
             *list(scaled_agent_shape.exterior.coords),
             color=(0, 0, 127),
             batch=self.batch,
         )
-        print(scaled_agent_shape.exterior.coords[0])
+
+        # anchor point defaults to first vertex, but should be in origo according to agent_shape
+        scale_offset = scaled_agent_shape.exterior.coords[0]
+        self.agent.anchor_position = (-scale_offset[0], -scale_offset[1])
+
+        self.agent.position = (self.window.width/2, self.window.height/2)
+
 
     ## NOTE: keep camera static and move agent
     # def update_agent(self, vessel: Vessel):
@@ -45,21 +53,28 @@ class visualizer:
     #     self.agent.position = (xpos, ypos)
     #     self.agent.rotation = -np.rad2deg(vessel.heading)
 
-    ## NOTE: keep the agent in the center of the screen and move everything else
+    def update_camerea_position(self, agent_position: np.ndarray):
+        """Updates the camera position, The coordinate defines the bottom left 'real world' location""" 
 
-    def update_agent(self, agent_heading: float, agent_shape: shapely.geometry.Polygon):
-        """Places the agent in the center of the screen considering the offset between
-            the shapely representation and the pylget shape of the object.
+        # Following the agent by moving the camera oppiste of agents movement,
+        # using the window offset to keep the agents position in the center for
+        # the screen
+        camera_x = -agent_position[0] * self.pixels_per_unit + self.window.width/2
+        camera_y = -agent_position[1] * self.pixels_per_unit + self.window.height/2
+        self.camera_position[0] = camera_x
+        self.camera_position[1] = camera_y
 
-            @Param
-                agent_heading: agents heading in rad
-                agent_shape: the agents shape defined as a shapely.geometry.Polygon
-        """
-        offset = agent_shape.exterior.coords[0]
-        xpos = self.window.width/2 + ( offset[0] * self.pixels_per_unit )
-        ypos = self.window.height/2 + (offset[1] * self.pixels_per_unit )
+    def update_agent(self, agent_heading: float, agent_position: np.ndarray):
+        """Update the agent"""
 
+        # xpos = self.camera_position[0] + (agent_position[0] * self.pixels_per_unit ) + self.window.width/2
+        # ypos = self.camera_position[1] + (agent_position[1] * self.pixels_per_unit ) + self.window.height/2
+        xpos = self.camera_position[0] + (agent_position[0] * self.pixels_per_unit )
+        ypos = self.camera_position[1] + (agent_position[1] * self.pixels_per_unit )
         self.agent.position = (xpos, ypos)
+        # print(f"camera_position: {self.camera_position}, vessel position: {xpos, ypos}") 
+
+        # self.agent.position = agent_position * self.pixels_per_unit + self.camera_position
         self.agent.rotation = -np.rad2deg(agent_heading)
 
 
@@ -73,16 +88,15 @@ class visualizer:
         self.bg_sprite.scale_x = ( self.window.width *2) / self.bg_sprite.width
         self.bg_sprite.scale_y = ( self.window.height *2) / self.bg_sprite.height
 
-    def update_background(self, agent_position: np.ndarray):
-        """Moves the background image the oppiste way of the agents position to 'simulate' the agent moving. """
-        scaled_agent_position = agent_position * self.pixels_per_unit
+    def update_background(self):
+        """Moves the background image the opposite way of the agents position
+        to 'simulate' the agent moving. """
 
-        # Move background image negative agent position to "simulate" the agent is moving
-        # "%" operation to keep the image within the bounds of the screen
-        new_bg_x = -1 * (scaled_agent_position[0] % self.window.width)
-        new_bg_y = -1 * (scaled_agent_position[1] % self.window.height)
+        new_bg_x = -(-self.camera_position[0] % self.window.width)
+        new_bg_y = -(-self.camera_position[1] % self.window.height)
         new_bg_pos = (new_bg_x, new_bg_y, 0)
-
+        # print(f"camerea: {self.camera_position}, bg: {new_bg_pos}")
+        
         self.bg_sprite.position = new_bg_pos
 
 
@@ -102,7 +116,11 @@ if __name__ == "__main__":
     bg_img_path = Path(__file__).resolve().parent.joinpath("graphics/bg.png")
     v.add_backround(bg_img_path)
     v.add_agent(vessel.boundary)
-    v.update_agent(vessel.heading, vessel.boundary)
+    v.update_agent(vessel.heading, vessel.position)
+    
+    # Add obstacle:
+    obst = CircularObstacle(np.array([0,0]), 1)
+    obst.init_pyglet_shape(v.pixels_per_unit, v.batch)
 
     game = Game()
     game.start_listner()
@@ -112,12 +130,17 @@ if __name__ == "__main__":
         if game.quit:
             break
 
-        vessel.step(game.action, 0.1)
-        v.update_agent(vessel.heading, vessel.boundary)
-        v.update_background(vessel.position)
+        vessel.step(game.action, 0.2)
+        v.update_camerea_position(vessel.position)
+        v.update_agent(vessel.heading, vessel.position)
+        v.update_background()
+        obst.update_pyglet_position(v.camera_position)
+        # obst.update_pyglet_position(v.,vessel.position, v.pixels_per_unit)
         v.update_screen()
         t +=1
-        # print(t)
+        print(f"vessel position: {vessel.position}, obstacle_pos {obst.position}")
+        # print(vessel.position)
+        # print(v.camera_position)
  
     # print(vessel.position)
     # print(v.agent.position)
