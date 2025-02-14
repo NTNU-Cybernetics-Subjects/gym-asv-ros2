571cb4f4c4d53f80924d6e45569d1be4d1af8171
Vegar <Vegar1201@live.no>
2025-02-14 16:42:29 +0100

    Working example of the simulator in ros.
    The code is not optimal but can serve as an interface of some sort.

---
+++ gym_asv_ros2/ros_nodes/manual_control_node.py
@@ -0,0 +1,81 @@
+# from gym_asv_ros2.obstacles import CircularObstacle
+# from gym_asv_ros2.vessel import Vessel
+# from gym_asv_ros2.visualization import Visualizer, BG_PMG_PATH
+import rclpy
+from rclpy.node import Node
+from blueboat_interfaces.msg import BlueboatActuatorInputs
+# from blueboat_interfaces import blueboat_actuator_inputs
+import numpy as np
+# from gym_asv_ros2.simulator import Game
+
+class ManualControlNode(Node):
+    
+    def __init__(self) -> None:
+        super().__init__("gym_asv_manual_controll_node")
+
+        self.publisher = self.create_publisher(
+            BlueboatActuatorInputs,
+            "/thruster_input",
+            10
+        )
+
+        self.action = [0.0,0.0]
+
+        self.get_logger().info("manual_control_node going into listening mode.")
+        self.start()
+
+
+    def start(self):
+        while True:
+            continue_loop = self.input_loop()
+            self.publish_action()
+            if not continue_loop:
+                pass # does not work to break beacause we are inside rclpy.spin()
+                # rclpy.shutdown()
+
+    def input_loop(self):
+        print(self.action, flush=True)
+        i = input(": ")
+        if "u" in i:
+            self.action[0] = 0.5
+
+        if "i" in i:
+            self.action[1] = 0.5
+
+        if "j" in i:
+            self.action[0] = -0.5
+
+        if "k" in i:
+            self.action[1] = -0.5
+
+        if len(i) <= 0:
+            self.action = [0.0, 0.0]
+
+        if "q" in i:
+            return False
+
+        return True
+
+    def publish_action(self):
+        # action = self.controller.action
+        msg = BlueboatActuatorInputs()
+        # msg.header.stamp = self.get_clock()
+        msg.stb_prop_force = self.action[0]
+        msg.port_prop_force = self.action[1]
+        self.publisher.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    manual_control_node = ManualControlNode()
+    rclpy.spin(manual_control_node)
+
+    manual_control_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ gym_asv_ros2/ros_nodes/simulator_node.py
@@ -0,0 +1,68 @@
+from gym_asv_ros2.obstacles import CircularObstacle
+from gym_asv_ros2.vessel import Vessel
+from gym_asv_ros2.visualization import Visualizer, BG_PMG_PATH
+import rclpy
+from rclpy.node import Node
+# from std_msgs.msg import String
+from blueboat_interfaces.msg import BlueboatActuatorInputs
+import numpy as np
+
+class SimulationNode(Node):
+
+    def __init__(self):
+        super().__init__("gym_asv_sim_node")
+        self.subscriber = self.create_subscription(
+            BlueboatActuatorInputs,
+            "/thruster_input",
+            self.thruster_input_callback,
+            10
+        )
+
+        self.viewer = Visualizer(1000, 1000)
+        init_vessel_state = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        self.vessel = Vessel(init_vessel_state, 1,1)
+
+        self.obstacles = []
+        self.action = np.array([0.0, 0.0])
+
+        self.init_world()
+
+        self.create_timer(0.01, self.timer_callback)
+
+    def init_world(self):
+
+        self.viewer.add_backround(BG_PMG_PATH)
+        self.viewer.add_agent(self.vessel.boundary)
+        circular_obst = CircularObstacle(np.array([10,10]), 1)
+        circular_obst.init_pyglet_shape(self.viewer.pixels_per_unit, self.viewer.batch)
+        self.obstacles.append(circular_obst)
+
+    def thruster_input_callback(self, msg: BlueboatActuatorInputs):
+        self.get_logger().info(f"got thruster msg: {msg.stb_prop_force, msg.port_prop_force}")
+        self.action[0] = msg.stb_prop_force
+        self.action[1] = msg.port_prop_force
+
+
+    def timer_callback(self):
+        self.get_logger().info("timer_callback called")
+        self.vessel.step(self.action, 0.1)
+        self.viewer.update_camerea_position(self.vessel.position)
+        self.viewer.update_agent(self.vessel.heading, self.vessel.position)
+        self.viewer.update_background()
+        for obst in self.obstacles:
+            obst.update_pyglet_position(self.viewer.camera_position)
+        self.viewer.update_screen()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    simulator_node = SimulationNode()
+    rclpy.spin(simulator_node)
+
+    simulator_node.destroy_node()
+    rclpy.shutdown()
+   
+
+
+
+
