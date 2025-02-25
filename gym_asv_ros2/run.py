4149eaba6157296adc403bb4a2a1c975e4c08540
Vegar <Vegar1201@live.no>
2025-02-25 13:18:40 +0100

    use the ranomized environmnet.

--- gym_asv_ros2/run.py
+++ gym_asv_ros2/run.py
@@ -4,24 +4,27 @@ from pathlib import Path
 
 import gymnasium as gym
 import numpy as np
+from datetime import datetime
 
 from stable_baselines3 import PPO
 import stable_baselines3.common.logger as sb3_logger
 from stable_baselines3.common.vec_env import SubprocVecEnv, VecMonitor
 
-from gym_asv_ros2.gym_asv.environment import Environment
+from gym_asv_ros2.gym_asv.environment import Environment, RandomDockEnv
 
 # from stable_baselines3.common.callbacks import BaseCallback
-from gym_asv_ros2.logging import FileStorage, TrainingCallback
+from gym_asv_ros2.logg import FileStorage, TrainingCallback, record_nested_dict
+from gym_asv_ros2.gym_asv.utils.manual_action_input import KeyboardListner
 
 # Better debugging
 from rich.traceback import install as install_rich_traceback
 install_rich_traceback()
 
-def make_env_subproc():
+def make_env_subproc(render):
 
     def _init():
-        env = Environment()
+        # env = Environment()
+        env = RandomDockEnv(render)
         return env
 
     return _init
@@ -38,51 +41,75 @@ def train(file_storage: FileStorage):
     #     "ent_coef": 0.01,  # Default 0.01
     # }
 
-    env_count = 1
-    env = SubprocVecEnv([make_env_subproc() for _ in range(env_count)])
+    env_count = 4
+    # total_timesteps = env_count * 1000000
+    total_timesteps = 10000
+    env = SubprocVecEnv([make_env_subproc(render=False) for _ in range(env_count)])
     env = VecMonitor(env)
     # env = Environment()
     
     model = PPO(
         "MlpPolicy",
         env=env,
-        # device="cpu",
+        device="cpu",
         verbose=True,
         # **hyperparams,
     )
 
-    model.set_logger(sb3_logger.configure(str(file_storage.info)))
-    model.learn(total_timesteps=100000, callback=TrainingCallback(str(file_storage.episode_summary)))
-    model.save(str(file_storage.agents.joinpath("agent")))
+    model.set_logger(sb3_logger.configure(str(file_storage.info), ["csv", "stdout", "tensorboard"]))
+
+    callback = callback=TrainingCallback(str(file_storage.episode_summary), str(file_storage.agents))
+    model.learn(total_timesteps=total_timesteps, callback=callback)
+
+    agent_file = str(file_storage.agents / "agent")
+    print(f"traing finished, saving agent to: {agent_file}")
+    model.save(agent_file)
 
 def enjoy(model_path: str):
-    env = Environment(render=True)
+    # env = SubprocVecEnv([ make_env_subproc(render=True) ])
+    env = RandomDockEnv(render=True)
     model = PPO.load(model_path, env=env)
 
-    obs, _ = env.reset()
-    done = False
-    while not done:
-        action, _states = model.predict(obs, deterministic=True)
-        obs, reward, done, truncated, info = env.step(action)
-        env.render()
+
+    while True:
+        obs, _ = env.reset()
+        done = False
+        while not done:
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, done, truncated, info = env.step(action)
+            # print(info["vessel_state"])
+            env.render()
+
+
 
 if __name__ == '__main__':
+    time_stamp = datetime.now().strftime("%d_%m_%Y__%H_%M_%S")
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "mode",
         help="TODO",
-        choices=["enjoy", "train"]
+        choices=["enjoy", "train", "play"]
+    )
+    parser.add_argument(
+        "--logid",
+        help="TOOD",
+        default=time_stamp
     )
     args = parser.parse_args()
 
-    file_storage = FileStorage("training")
+    # print(args.logid)
+    file_storage = FileStorage("training", args.logid)
 
-    # model = "ppo_test"
     if args.mode == "enjoy":
         enjoy(str(file_storage.agents / "agent"))
+
     elif args.mode == "train":
         start_time = time.time()
         train(file_storage)
         end_time = time.time()
-        print(f"Elapsed time: {end_time - start_time}")
+        print(f"elapsed time: {end_time - start_time}")
+
+    # elif args.mode == "play":
+    #     play()
 
