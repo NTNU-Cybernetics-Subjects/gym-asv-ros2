2e56849a39be7b9b050420d199c59bc99c1c3cba
Vegar <Vegar1201@live.no>
2025-03-03 14:45:42 +0100

    Add a lidar simulation with visualization using the lineEntity.

---
+++ gym_asv_ros2/gym_asv/sensors.py
@@ -0,0 +1,127 @@
+from abc import abstractmethod
+from sys import intern
+import numpy as np
+import pyglet
+import shapely
+import gym_asv_ros2.gym_asv.utils.geom_utils as geom
+from gym_asv_ros2.gym_asv.entities import BaseEntity, CircularEntity, LineEntity
+import time
+
+# testing
+from gym_asv_ros2.gym_asv.visualization import TestCase
+
+class BaseSensor:
+
+    @abstractmethod
+    def _init_visual(self):
+        pass
+
+    @abstractmethod
+    def _update_visual(self):
+        pass
+
+
+class LidarSimulator:
+    
+    def __init__(self, max_range: float, num_rays: int):
+        self.max_range = max_range # [m]
+
+        self.num_rays = num_rays
+        self.angle_range = np.array([-np.pi/2, np.pi/2]) # Start angle, and end angle 
+
+        self.angles = np.linspace(self.angle_range[0], self.angle_range[1], self.num_rays) # TODO: handle dupicated angle if we are going around somehow [0, 2*np.pi]
+        self._ray_lines = [ LineEntity(np.array([0.0,0.0]), np.array([0.0, 0.0]), color=(127,0,0)) for _ in range(self.num_rays)]
+
+
+    def update_ray_line(self, i, start_point: shapely.Point | None = None, end_point: shapely.Point | None = None):
+        
+        update = False
+        if start_point:
+            self._ray_lines[i].position = np.array(*start_point.coords)
+            update = True
+
+        if end_point:
+            self._ray_lines[i].end_position = np.array(*end_point.coords)
+            update = True
+
+        if update:
+            # Boundary object is immutable, need to calculate again when it changes
+            self._ray_lines[i].init_boundary()
+
+
+
+    def sense(self, position: np.ndarray, heading: float, obstacles: list[BaseEntity]):
+        """TODO"""
+
+        lidar_readings = np.full(self.num_rays, float(self.max_range))
+
+        start_point = shapely.Point(position)
+
+        for i, angle in enumerate(self.angles):
+            true_angle = angle + heading
+
+            current_closet_point = shapely.Point(
+                start_point.x + self.max_range * np.cos(true_angle),
+                start_point.y + self.max_range * np.sin(true_angle)
+            )
+            self.update_ray_line(i, start_point, current_closet_point)
+
+            for obj in obstacles:
+                intersection = self._ray_lines[i].boundary.intersection(obj.boundary)
+
+                if intersection.is_empty:
+                    continue
+
+                if isinstance(intersection, shapely.Point):
+                    if start_point.distance(intersection) < start_point.distance(current_closet_point):
+                        current_closet_point = intersection
+
+                if isinstance(intersection, shapely.MultiPoint):
+                    for point in intersection.geoms:
+                        if start_point.distance(point) < start_point.distance(current_closet_point):
+                            current_closet_point = point
+                 
+            self.update_ray_line(i, end_point=current_closet_point)
+            lidar_readings[i] = start_point.distance(current_closet_point)
+
+        return lidar_readings
+
+
+if __name__ == "__main__":
+    obst1 = CircularEntity(np.array([10,10]), 1)
+    # obst2 = CircularEntity(np.array([0,10]), 1)
+
+    lidar = LidarSimulator(20, 2)
+    game_test = TestCase([obst1])
+
+
+    pyglet_lines = []
+    def setup():
+        for ray_line in lidar._ray_lines:
+            # print(f"initializing ray_lines {ray_line}")
+            ray_line.init_pyglet_shape(game_test.viewer.pixels_per_unit, game_test.viewer.batch)
+
+
+    def update():
+        lidar_readings = lidar.sense(game_test.vessel.position, game_test.vessel.heading, game_test.obstacles)
+        # Update ray_lines
+        for line in lidar._ray_lines:
+            line.update_pyglet_position(game_test.viewer.camera_position, game_test.viewer.pixels_per_unit)
+        
+        print(f"points: {[ray_line.end_position for ray_line in lidar._ray_lines]}, readings: {lidar_readings}")
+        # for i, ray_line in enumerate(lidar._ray_lines):
+        #     print(f"at {ray_line.end_position} distance is {lidar_readings[i]}")
+
+    game_test.game_loop(setup=setup,update=update)
+
+
+
+
+
+
+
+
+
+
+
+
