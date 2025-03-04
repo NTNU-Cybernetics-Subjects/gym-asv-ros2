d97242fee25f1438e712ef11aefa18b9a1ce3d11
Vegar <Vegar1201@live.no>
2025-03-04 12:57:47 +0100

    Remove lidar sensor class.

--- gym_asv_ros2/gym_asv/vessel.py
+++ gym_asv_ros2/gym_asv/vessel.py
@@ -6,8 +6,8 @@ import numpy as np
 import shapely
 from numpy import linalg
 from scipy.integrate import solve_ivp
-from entities import BaseEntity
 
+from gym_asv_ros2.gym_asv.entities import BaseEntity
 import gym_asv_ros2.gym_asv.utils.geom_utils as geom
 
 class VesselParamters(NamedTuple):
@@ -38,17 +38,6 @@ class ThrusterParams(NamedTuple):
 
     max_forward_force: float = 55.21
     max_backward_force: float = 27.5
-
-
-class LidarSim:
-
-    def __init__(self) -> None:
-        pass
-
-    def simulate_sensor(self, p0_position: np.ndarray, obstacles: list[BaseEntity] ) -> None:
-        pass
-        
-
 
 
 class Vessel:
