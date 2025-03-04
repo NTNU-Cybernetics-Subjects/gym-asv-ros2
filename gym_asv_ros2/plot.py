1c3132c4c371dce6f14aa66bf510c6073fd8b83c
Vegar <Vegar1201@live.no>
2025-03-04 12:57:07 +0100

    Plot reward from 4 envs.

--- gym_asv_ros2/plot.py
+++ gym_asv_ros2/plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-dir = "/home/hurodor/Dev/blue_boat_ws/src/gym_asv_ros2/training/closure_reward_1M/episode_summary/"
+dir = "/home/hurodor/Dev/blue_boat_ws/src/gym_asv_ros2/training/dock_obst_test/episode_summary/"
 in_file = dir + "progress.csv"
 out_file = dir + "processed.csv"
 
@@ -16,11 +16,21 @@ sorted.to_csv(out_file, index=False)
 
 env_1 = df[df["env_id"] == 0]
 env_2 = df[df["env_id"] == 1]
+env_3 = df[df["env_id"] == 2]
+env_4 = df[df["env_id"] == 3]
 
-reward = df["episode/r"].to_numpy()
-t = np.arange(0, len(reward))
-plt.plot(t, reward)
+envs = [env_1, env_2, env_3, env_4]
 
+for i, env in enumerate(envs):
+    reward = env["episode/r"].to_numpy()
+    t = np.arange(0, len(reward))
+    plt.plot(t, reward, label=f"env_{i}")
+
+
+# reward = df["episode/r"].to_numpy()
+# t = np.arange(0, len(reward))
+# plt.plot(t, reward)
+plt.legend()
 plt.show()
 
 
