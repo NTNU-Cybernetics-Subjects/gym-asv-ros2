6fb9b23861f0c53cfc24ad066c05249ffcad1194
Vegar <Vegar1201@live.no>
2025-04-08 17:04:57 +0200

    Add state msg and use seperate topics for lidar & state.

--- gym_asv_ros2/ros/agent_node.py
+++ gym_asv_ros2/ros/agent_node.py
@@ -1,9 +1,10 @@
 
+from gym_asv_ros2.gym_asv.environment import RandomGoalWithDockObstacle
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import Float32MultiArray
-from blueboat_interfaces.msg import BlueboatActuatorInputs
+from blueboat_interfaces.msg import BlueboatActuatorInputs, BlueboatState
 
 import numpy as np
 from stable_baselines3 import PPO
@@ -19,34 +20,82 @@ class AgentNode(Node):
         if not agent_file:
             raise FileExistsError(f"{agent_file} does not exists.")
 
-        self.observation_sub = self.create_subscription(
+
+        # Lidar
+        self.lidar_sub = self.create_subscription(
             Float32MultiArray,
-            "/observation_space",
-            self.observation_callback,
+            "/blueboat/lidar",
+            self.lidar_sub_callback,
             10
         )
+        self.real_lidar_messurments = np.ones((41,))
+        self.last_lidar_recived = self.get_clock().now()
+
+        # Vessel
+        self.vessel_state_sub = self.create_subscription(
+            BlueboatState,
+            "/blueboat/state",
+            self.state_sub_callback,
+            10
+        )
+        self.real_vessel_state = np.zeros((6,))
+        self.last_vessel_state_recived = self.get_clock().now()
+
+        # Action
         self.action_pub = self.create_publisher(
             BlueboatActuatorInputs,
-            "/thruster_input",
+            "/blueboat/u",
             10
         )
 
         self.agent = PPO.load(agent_file)
 
-    def observation_callback(self, obs_msg: Float32MultiArray):
+        # Environemnt
+        # self.env = RandomGoalWithDockObstacle(render_mode=None)
+        # self.simulated_observation, _ = self.env.reset()
 
-        self.get_logger().info(f"Recived observation: {obs_msg}")
-        observation = np.array([ obs_msg.data ])
-        self.get_logger().info(f"Formating observation: {observation}")
+        self.run_fequency = 0.01
+        self.create_timer(self.run_fequency, self.run)
 
+
+    # FIXME: This needs to be modified according to the actual sensor data
+    def lidar_sub_callback(self, msg: Float32MultiArray):
+
+        self.real_lidar_messurments = np.array(msg.data)
+
+        self.last_lidar_recived = self.get_clock().now()
+
+    def state_sub_callback(self, msg: BlueboatState):
+
+        self.real_vessel_state[0] = msg.x
+        self.real_vessel_state[1] = msg.y
+        self.real_vessel_state[2] = msg.yaw
+        self.real_vessel_state[3] = msg.surge
+        self.real_vessel_state[4] = msg.sway
+        self.real_vessel_state[5] = msg.yaw_r
+
+        self.last_vessel_state_recived = self.get_clock().now()
+
+
+    def run(self):
+
+        # TODO: Merge with Env?
+        observation = np.concatenate([self.real_vessel_state, self.real_lidar_messurments])
+
+        # Predict from real messurments?
         action, _states = self.agent.predict(observation, deterministic=True)
+        action = action.flatten()
+
+        # self.simulated_observation, reward, done, truncated, info = self.env.step(action)
+        # if done or truncated:
+        #     self.get_logger().info("Simulations reached goal")
 
         # Publish action
-        action_msg = BlueboatActuatorInputs()
-        action_msg.stb_prop_force = float(action[0])
-        action_msg.port_prop_force = float(action[1])
+        action_msg = BlueboatActuatorInputs(
+            stb_prop_force=float(action[0]),
+            port_prop_force=float(action[1])
+        )
         self.action_pub.publish(action_msg)
-
 
 def main(args=None):
     rclpy.init(args=args)
--- gym_asv_ros2/ros/simulator_node.py
+++ gym_asv_ros2/ros/simulator_node.py
@@ -3,77 +3,93 @@ import rclpy
 from rclpy.node import Node
 from rclpy.duration import Duration
 
-from blueboat_interfaces.msg import BlueboatActuatorInputs
+from blueboat_interfaces.msg import BlueboatActuatorInputs, BlueboatState
 from std_msgs.msg import Float32MultiArray
 
 import numpy as np
 from gym_asv_ros2.gym_asv.entities import CircularEntity
 from gym_asv_ros2.gym_asv.vessel import Vessel
 from gym_asv_ros2.gym_asv.visualization import Visualizer, BG_PMG_PATH
-from gym_asv_ros2.gym_asv.environment import RandomDockEnv
+from gym_asv_ros2.gym_asv.environment import RandomGoalWithDockObstacle
 
 class SimulationNode(Node):
 
     def __init__(self):
         super().__init__("gym_asv_sim_node")
 
-        self.aciton_sub = self.create_subscription(
+        self.action_sub = self.create_subscription(
             BlueboatActuatorInputs,
-            "/thruster_input",
+            "/blueboat/u",
             self.thruster_input_callback,
             10
         )
 
-        self.observation_pub = self.create_publisher(
-            Float32MultiArray,
-            "/observation_space",
+        self.vessel_state_pub = self.create_publisher(
+            BlueboatState,
+            "/blueboat/state",
             10
         )
+
+        self.lidar_pub = self.create_publisher(
+            Float32MultiArray,
+            "/blueboat/lidar",
+            10
+        )
+
         # Initialize env
-        self.env = RandomDockEnv(render_mode="human")
+        self.env = RandomGoalWithDockObstacle(render_mode="human")
         self.env.reset()
         self.env.render()
 
+        # Action
         self.action = np.array([0.0, 0.0])
-        self.wait_for_action_duration = Duration(seconds=5)
-        self.last_action_resived = self.get_clock().now() - self.wait_for_action_duration
 
-        self.create_timer(0.01, self.render_callback)
-        self.create_timer(0.1, self.publish_observation)
+        render_frequence = 0.01
+        observation_pub_frequence = 0.1
+        self.create_timer(render_frequence, self.render_callback)
+        self.create_timer(observation_pub_frequence, self.publish_observation)
 
     def __del__(self):
         self.env.close()
 
     def render_callback(self):
-
-        current_time = self.get_clock().now()
-        if current_time > self.last_action_resived + self.wait_for_action_duration:
-            self.get_logger().info(
-                "It is more than 10 seconds since we got an thrust update.\
-                    setting Thrust to 0 for safty reasons")
-            self.action[0], self.action[1] = 0.0, 0.0
         
         observation, reward, done, truncated, info = self.env.step(self.action)
         self.last_observation = observation
 
-        if done:
+        if done or truncated:
             self.env.reset()
             self.publish_observation()
 
         self.env.render()
 
     def publish_observation(self):
-        # Publish observation
-        observation_msg = Float32MultiArray()
-        observation_msg.data = self.last_observation.flatten().tolist()
-        self.observation_pub.publish(observation_msg)
+
+        # Publish state
+        sim_state = self.last_observation.flatten()[:6]
+        sim_state_msg = BlueboatState(
+            x=sim_state[0],
+            y=sim_state[1],
+            yaw=sim_state[2],
+            surge=sim_state[3],
+            sway=sim_state[4],
+            yaw_r=sim_state[5]
+        )
+        self.vessel_state_pub.publish(sim_state_msg)
+
+        # Publish lidar
+        lidar_messurments = self.last_observation.flatten()[6:]
+        lidar_msg = Float32MultiArray(
+            data=lidar_messurments.tolist()
+        )
+        self.lidar_pub.publish(lidar_msg)
 
 
     def thruster_input_callback(self, msg: BlueboatActuatorInputs):
-        self.get_logger().info(f"got thruster msg: {msg.stb_prop_force, msg.port_prop_force}")
+        self.get_logger().debug(f"got thruster msg: {msg.stb_prop_force, msg.port_prop_force}")
+
         self.action[0] = msg.stb_prop_force
         self.action[1] = msg.port_prop_force
-        self.last_action_resived = self.get_clock().now()
 
 
 def main(args=None):
